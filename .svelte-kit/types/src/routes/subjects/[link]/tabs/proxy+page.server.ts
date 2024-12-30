2fc1a7655e845396806a44d4114f7c53c122e9bd
Robinnoss27 <robinnoss27@gmail.com>
2024-12-30 15:54:49 +0100

    Private,community content, Odstránenie nevhodných súborov z repozitára

--- .svelte-kit/types/src/routes/subjects/[link]/tabs/proxy+page.server.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import type { PageServerLoad } from './$types';
-
-export const load = async ({ locals }: Parameters<PageServerLoad>[0]) => {
-    return {
-        isAdmin: locals.isAdmin || false, // Nastaví isAdmin z locals
-    };
-};
--- .svelte-kit/types/src/routes/subjects/[link]/tabs/subtabs/content/proxy+page.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { PageServerLoad } from './$types';
-
-export const load = async ({ locals }: Parameters<PageServerLoad>[0]) => {
-    console.log(locals.isAdmin);
-    return {
-        isAdmin: locals.isAdmin || false, // Nastaví isAdmin z locals
-    };
-};
